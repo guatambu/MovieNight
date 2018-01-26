68141b2f6615d4dfd4147bd5533e715010b1127a
Kelly Johnson <kellyjohnson@Kellys-MacBook-Pro.local>
2018-01-26 13:42:17 -0800

    customized and wired up YouTableViewController prototype cells

---
+++ MovieNight/Controllers/YouTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  YouTableViewCell.swift
+//  MovieNight
+//
+//  Created by Michael Guatambu Davis on 1/26/18.
+//  Copyright © 2018 leme group. All rights reserved.
+//
+
+import UIKit
+
+class YouTableViewCell: UITableViewCell {
+    
+    //MARK: Properties
+    @IBOutlet weak var movieTitleLabel: UILabel!
+    @IBOutlet weak var releaseDateLabel: UILabel!
+    @IBOutlet weak var briefMovieDescriptionLabel: UILabel!
+    @IBOutlet weak var moviePosterImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
